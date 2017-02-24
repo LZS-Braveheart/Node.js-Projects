2fabd049bb52cb6b2d3d241fe9e458553cdff726
shanguagua <lzsbraveheart@163.com>
2017-02-24 17:11:09 +0800

    mongoose

--- InstrumentCollections/demos8/day8/03.js
+++ InstrumentCollections/demos8/day8/03.js
@@ -2,7 +2,7 @@
  * Created by Danny on 2015/9/29 10:18.
  */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://127.0.0.1:27017/DB/shanguagua');
 
 var db = mongoose.connection;
 db.once('open', function (callback) {
@@ -11,9 +11,9 @@ db.once('open', function (callback) {
 
 //博客的结构
 var animalSchema = new mongoose.Schema({
-    "name" : String,
+    "name" : String,     
     "type" : String
-});
+});    
 
 animalSchema.methods.zhaotonglei = function(callback){
     this.model('Animal').find({"type":this.type},callback);
@@ -21,10 +21,10 @@ animalSchema.methods.zhaotonglei = function(callback){
 
 var Animal = mongoose.model('Animal', animalSchema);
 
-//Animal.create({"name":"汤姆","type":"猫"});
-//Animal.create({"name":"咪咪","type":"猫"});
-//Animal.create({"name":"小白","type":"狗"});
-//Animal.create({"name":"snoopy","type":"狗"});
+// Animal.create({"name":"汤姆","type":"猫"});
+// Animal.create({"name":"咪咪","type":"猫"});
+// Animal.create({"name":"小白","type":"狗"});
+// Animal.create({"name":"snoopy","type":"狗"});
 
 Animal.findOne({"name":"小白"},function(err,result){
     var dog = result;
