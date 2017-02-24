1c32a2ba30c9d635bd5309f49b7168634a6eecd9
shanguagua <lzsbraveheart@163.com>
2017-02-24 21:50:59 +0800

    demos8

--- InstrumentCollections/demos8/studentmanagement/models/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/xueshengguanli');
-
-var db = mongoose.connection;
-db.once('open', function (callback) {
-    console.log("数据库成功打开");
-});
-
-module.exports = db;
