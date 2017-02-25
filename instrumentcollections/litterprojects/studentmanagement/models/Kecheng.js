725c45cb8948e16eb591f4ed5e54151f881d84ab
shanguagua <lzsbraveheart@163.com>
2017-02-25 15:49:15 +0800

    1

---
+++ instrumentcollections/litterprojects/studentmanagement/models/Kecheng.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Danny on 2015/9/29 14:50.
+ */
+                  
+var mongoose = require('mongoose');
+    
+//schema   
+var kechengSchema = new mongoose.Schema({
+    "kid"  : Number,
+    "name" : String,
+    "students" : [Number]       //这个数组存放的是学生的sid             
+});
+
+//索引
+kechengSchema.index({ "kid": 1});                                                         
+   
+kechengSchema.statics.tianjiaxuesheng = function(kidarray,sid,callback){
+    for(var i = 0 ; i < kidarray.length ; i++){
+    	// "kid"：课程id,"sid"：学生id
+        Kecheng.update({"kid":kidarray[i]},{$push :{"students":sid}},function(){
+            console.log("课程添加报名学生成功");
+        })
+    }
+}
+
+//model
+var Kecheng = mongoose.model("Kecheng",kechengSchema);
+
+module.exports = Kecheng;
